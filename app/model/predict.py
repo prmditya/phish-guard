4bd9ff3c98dc6bd9b7af4ac4ad28c329c76afc99
Thoriq Kusuma <thoriqk2@gmail.com>
2024-11-23 11:10:58 +0700

    feat(deploy): deploy model and also integrate it with the website

--- app/model/predict.py
+++ app/model/predict.py
@@ -1,13 +1,21 @@
-import numpy as np
-import pickle
-from flask import request, jsonify
+import pandas as pd
+import json
+from joblib import load
 from app.utils.features_extraction import extract_features
 
-with open('phishing_model.pkl', 'rb') as file:
-    model = pickle.load(file)
+model = load('app/model/url_classification_rf.joblib')
 
-def predict(features):
-  prediction = model.predict(features)[0]
-  
+def predict(url):
+  features = extract_features(url['url'])
+
+  features_df = pd.DataFrame([features])  # Wrap in a list to ensure it's a row
+  flat_data = {key: value[0] if isinstance(value, list) else value for key, value in features.items()}
+  print(json.dumps(flat_data, indent=2))
+
+  # Ensure column names match the model's expected features (if trained with names)
+  # Convert the DataFrame to a numpy array to drop feature names if needed
+  prediction = model.predict(features_df.to_numpy())[0]  # sklearn handles DataFrame if names match
+  print(f'Prediction: {prediction}')
+
   result = 'Phishing' if prediction == 1 else 'Safe'
   return result
